ae09bbaf51821101fece32f056cabbe805475d51
Eduardo <eduardojor77@gmail.com>
2024-04-09 14:41:04 -0300

    describe HelloWorld and removing comments

--- test/HelloWorld.test.ts
+++ test/HelloWorld.test.ts
@@ -6,12 +6,8 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import hre from "hardhat";
 
-describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
+describe("HelloWorld", function () {
   async function deployFixture() {
-    // Contracts are deployed using the first signer/account by default
     const [owner, otherAccount] = await hre.ethers.getSigners(); // pega carteiras de teste
 
     const HelloWorld = await hre.ethers.getContractFactory("HelloWorld"); // design-patern para criação de objetos
