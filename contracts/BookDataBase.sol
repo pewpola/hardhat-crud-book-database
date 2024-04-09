db9b395361ac1a33f1f3a02d414354978b310739
Eduardo <eduardojor77@gmail.com>
2024-04-09 10:18:51 -0300

    included BookDataBase.sol

---
+++ contracts/BookDataBase.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+contract BookDataBase {
+    
+    struct Book {
+        string title;
+        uint16 year;
+    }
+
+    uint32 private nextId = 0;
+    mapping (uint32 => Book) public books;
+
+    address private immutable owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function compare(string memory str1, string memory str2) private pure returns (bool) {
+        bytes memory arrA = bytes(str1);
+        bytes memory arrB = bytes(str2);
+        return arrA.length == arrB.length && keccak256(arrA) == keccak256(arrB);
+    }
+
+    function addBook(Book memory newBook) public {
+        nextId++;
+        books[nextId] = newBook;
+    }
+
+    function editBook(uint32 id, Book memory newBook) public {
+        Book memory oldBook = books[id];
+
+        if (!compare(oldBook.title, newBook.title) && !compare(newBook.title,"")) {
+            books[id].title = newBook.title;
+        }
+
+        if (oldBook.year != newBook.year && newBook.year > 0) {
+            books[id].year = newBook.year;
+        }
+    }
+
+    function removeBook(uint32 id) public restricted {
+        delete books[id];
+    }
+
+    modifier restricted() {
+        require(owner == msg.sender, "You don't have permission.");
+        _;
+    }
+}
