3da35996a6e67c7a11acf422a9731b164120e8af
Eduardo <eduardojor77@gmail.com>
2024-04-12 00:37:44 -0300

    importing solidity-docgen

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,7 @@ dotenv.config();
 
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "solidity-docgen";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
